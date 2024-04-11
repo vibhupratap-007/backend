d48ea6ef664d9ae4640ff557a3b62080205fc85f
vibhupratap-007 <vibhupratap007@gmail.com>
2024-04-11 16:04:55 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,42 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+const githubData = {
+    "login": "vibhupratap",
+    "id": 19766167,
+    "node_id": "MDQ6VXNlcjE5NzY2MTY3",
+    "avatar_url": "https://avatars.githubusercontent.com/u/19766167?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/vibhupratap",
+    "html_url": "https://github.com/vibhupratap",
+    "followers_url": "https://api.github.com/users/vibhupratap/followers",
+    "following_url": "https://api.github.com/users/vibhupratap/following{/other_user}",
+    "gists_url": "https://api.github.com/users/vibhupratap/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/vibhupratap/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/vibhupratap/subscriptions",
+    "organizations_url": "https://api.github.com/users/vibhupratap/orgs",
+    "repos_url": "https://api.github.com/users/vibhupratap/repos",
+    "events_url": "https://api.github.com/users/vibhupratap/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/vibhupratap/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Vibhu Pratap",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 7,
+    "public_gists": 0,
+    "followers": 1,
+    "following": 0,
+    "created_at": "2016-06-05T22:04:01Z",
+    "updated_at": "2020-05-30T00:57:23Z"
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -18,6 +53,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send("<h2>chai aur code </h2>")
+})
+
+app.get('/github', (req, res) =>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {

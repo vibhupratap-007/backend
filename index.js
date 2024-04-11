251b696f8b7413e33ebc907b6e2cffdf9e10bc87
Vibhu Pratap <vibhupratap@Vibhus-MacBook-Air.local>
2024-04-11 12:34:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('vibhucom')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login at vibhu and code</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send("<h2>chai aur code </h2>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
